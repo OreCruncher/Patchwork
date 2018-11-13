863084892b3bb341dfe1c682b33250e5bf196dc7
OreCruncher <tedjkaiser@gmail.com>
2018-11-13 11:53:25 -0800

    Update version URL for repo rename

--- src/main/java/org/orecruncher/patchwork/ModInfo.java
+++ src/main/java/org/orecruncher/patchwork/ModInfo.java
@@ -31,7 +31,7 @@ public final class ModInfo {
 	public static final String VERSION = "@VERSION@";
 	public static final String MINECRAFT_VERSIONS = "[1.12.2,)";
 	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2768,);required-after:orelib@[3.5,)";
-	public static final String UPDATE_URL = "https://raw.githubusercontent.com/OreCruncher/Doodads/master/version.json";
+	public static final String UPDATE_URL = "https://raw.githubusercontent.com/OreCruncher/Patchwork/master/version.json";
 	public static final String FINGERPRINT = "7a2128d395ad96ceb9d9030fbd41d035b435753a";
 
 	private ModInfo() {
